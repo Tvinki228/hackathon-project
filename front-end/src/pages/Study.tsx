85622bd2eb20645bf59b2058684ab3896faa2e1b
introvenko <volodymyry94@gmail.com>
2025-08-30 14:26:32 +0200

    Make some changes

--- front-end/src/pages/Study.tsx
+++ front-end/src/pages/Study.tsx
@@ -17,21 +17,6 @@ export default function Study() {
           <a href="#">Subjects</a>
           <a href="#">Study Sets</a>
         </nav>
-
-        <div className={styles.userSection}>
-          <button className={styles.helpBtn}>
-            <svg viewBox="0 0 256 256">
-              <path d="M140,180a12,12,0,1,1-12-12A12,12,0,0,1,140,180ZM128,72c-22.06,0-40,16.15-40,36v4a8,8,0,0,0,16,0v-4c0-11,10.77-20,24-20s24,9,24,20-10.77,20-24,20a8,8,0,0,0-8,8v8a8,8,0,0,0,16,0v-.72c18.24-3.35,32-17.9,32-35.28C168,88.15,150.06,72,128,72Zm104,56A104,104,0,1,1,128,24,104.11,104.11,0,0,1,232,128Zm-16,0a88,88,0,1,0-88,88A88.1,88.1,0,0,0,216,128Z"></path>
-            </svg>
-          </button>
-          <div
-            className={styles.avatar}
-            style={{
-              backgroundImage:
-                'url("https://lh3.googleusercontent.com/aida-public/AB6AXu...")',
-            }}
-          />
-        </div>
       </header>
 
       <main className={styles.main}>
@@ -46,7 +31,9 @@ export default function Study() {
               <path d="M14.5 21H6a2 2 0 0 1-2-2V5a2..."></path>
             </svg>
           </div>
-          <p className={styles.uploadTitle}>Slingshot your notes into the void!</p>
+          <p className={styles.uploadTitle}>
+            Slingshot your notes into the void!
+          </p>
           <p className={styles.uploadDesc}>
             Or click to launch your files (PDF, notes, carrier pigeon)
           </p>
@@ -78,4 +65,3 @@ export default function Study() {
     </div>
   );
 }
-
